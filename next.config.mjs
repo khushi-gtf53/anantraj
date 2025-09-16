30e3239cfa02c583fa84c7c04ea312b1c7062150
khushi-gtf53 <khushi.kashyap@gtftechnologies.com>
2025-09-16 09:40:06 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true, // ✅ sabhi images normal <img> ke tarah serve hongi
-  },
-  // basePath: '/anantraj',
-  // assetPrefix: '/anantraj/',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export', 
 };
 
 export default nextConfig;
